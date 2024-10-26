47b9c92e93bf7ad788a0e3549994f461f3ba73be
MUHAMMAD ALI <164537137+muhammadali9867@users.noreply.github.com>
2024-10-26 14:15:50 +0500

    Add files via upload

---
+++ Number Gussing Game/README.md
@@ -0,0 +1,2 @@
+# cli-number1
+This is a based number guessing game:
---
+++ Number Gussing Game/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node 
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 +1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuesseNumber",
+    type: "number",
+    message: "please guess a number between 1-6:",
+  },
+]);
+if (answers.userGuesseNumber === randomNumber) {
+  console.log("congtratulation! you guessed right.");
+} else {
+  console.log("you guessed wrong number");
+}
