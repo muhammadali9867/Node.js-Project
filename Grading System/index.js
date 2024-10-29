9484652242514beca5c770fd71c9093b0d8149ff
MUHAMMAD ALI <164537137+muhammadali9867@users.noreply.github.com>
2024-10-29 21:24:34 +0500

    Add files via upload

---
+++ Grading System/index.js
@@ -0,0 +1,28 @@
+// Grading System
+let maxMarks = 850;
+let obtMarks = 640;
+let percent = (obtMarks * 100) / maxMarks;
+console.log(percent);
+if (percent >= 80 && percent <= 100) {
+    console.log("Exceptional, Your Grade is A1");
+}
+else if (percent >= 70 && percent <= 79) {
+    console.log("Excellent, Your Grade is A");
+}
+else if (percent >= 60 && percent <= 69) {
+    console.log("Very good, Your Grade is B");
+}
+else if (percent >= 50 && percent <= 59) {
+    console.log("Good, Your Grade is C");
+}
+else if (percent >= 40 && percent <= 49) {
+    console.log("Very Fair, Your Grade is D");
+}
+else if (percent >= 33 && percent <= 39) {
+    console.log("Fair, Your Grade is E");
+}
+else {
+    console.log("Fail, Your Grade is F");
+}
+;
+export {};
